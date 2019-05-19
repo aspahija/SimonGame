9995fc910dd85a9fb4b5fb25c306c0e0cc43e301
aspahija <ante.spahija@gmail.com>
2019-05-19 18:55:04 +0200

    add animations to clicks

--- game.js
+++ game.js
@@ -1,20 +1,17 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
-
 var userClickedPattern = [];
-
 
 $(".btn").click(function() {
 
   var userChosenColour = $(this).attr("id");
-
   userClickedPattern.push(userChosenColour);
 
   playSound(userChosenColour);
 
+  animatePress(userChosenColour);
 });
-
 
 function nextSequence() {
 
@@ -25,13 +22,18 @@ function nextSequence() {
   $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
 
   playSound(randomChosenColour);
-
 }
 
-
 function playSound(name) {
-
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
+}
 
+function animatePress(currentColour) {
+
+  $("#" + currentColour).addClass("pressed");
+
+  setTimeout(function(){
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
 }
